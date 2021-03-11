be3cab57782b07740ec68aaea380bf443bf0e8f6
Lola <lola@egherman.com>
2021-03-11 13:21:45 -0600

    finish code (I think) add readme, requirements

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tshirt-cannon
 Cognitoforms -> shipstation webhook microservice thing
+
+## Environment variables
+###Required: `SHIPSTATION_API_KEY`, `SHIPSTATION_API_SECRET`
+###Optional, set to sensible default values (for CodeDay use at least): `SHIRT_S_SKU`, `SHIRT_M_SKU`, `SHIRT_L_SKU`, `SHIRT_XL_SKU`
---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, request
+from shipstation.api import ShipStation, ShipStationOrder, ShipStationAddress, ShipStationItem
+
+app = Flask(__name__)
+
+ss = ShipStation(key=os.getenv('SHIPSTATION_API_KEY'), secret=os.getenv('SHIPSTATION_API_SECRET'))
+shirts = {
+    'S':  ShipStationItem(sku=os.getenv('SHIRT_S_SKU', '605925771853')),
+    'M':  ShipStationItem(sku=os.getenv('SHIRT_M_SKU', '605925771860')),
+    'L':  ShipStationItem(sku=os.getenv('SHIRT_L_SKU', '605925771877')),
+    'XL': ShipStationItem(sku=os.getenv('SHIRT_XL_SKU', '605925771884'))
+}
+
+
+@app.route('/send')
+def tshirt_cannon():
+    body = request.json
+    print(body)
+    ss_order = ShipStationOrder(order_number=f'tshirt-cannon-{str(body["Entry"]["Number"])}')
+    shipping_address = ShipStationAddress(
+        name=body['YourName2']['FirstAndLast'],
+        street1=body['Address']['Line1'],
+        street2=body['Address']['Line2'],
+        street3=body['Address']['Line3'],
+        city=body['Address']['City'],
+        state=body['Address']['State'],
+        postal_code=body['Address']['PostalCode'],
+        country=body['Address']['Country']
+    )
+    ss_order.set_billing_address(shipping_address)
+    ss_order.set_shipping_address(shipping_address)
+    ss_order.add_item(shirts[body['TShirtSize']])
+    ss.add_order(ss_order)
+    return
