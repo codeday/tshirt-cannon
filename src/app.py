8ca8b7c19e2ba167ec8c7ccf1fcdc342fcbf3c79
Lola <lola@egherman.com>
2021-03-12 15:47:11 -0600

    add quantities and crap just to get it up to date for lynnes pull request

--- src/app.py
+++ src/app.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask, request
-from shipstation.api import ShipStation, ShipStationOrder, ShipStationAddress, ShipStationItem
+from shipstation.api import ShipStation, ShipStationOrder, ShipStationAddress, ShipStationItem, ShipStationWeight
 
+no_weight = ShipStationWeight(units='ounces',value=0)
 app = Flask(__name__)
-
 ss = ShipStation(key=os.getenv('SHIPSTATION_API_KEY'), secret=os.getenv('SHIPSTATION_API_SECRET'))
 shirts = {
-    'S':  ShipStationItem(sku=os.getenv('SHIRT_S_SKU', '605925771853')),
-    'M':  ShipStationItem(sku=os.getenv('SHIRT_M_SKU', '605925771860')),
-    'L':  ShipStationItem(sku=os.getenv('SHIRT_L_SKU', '605925771877')),
-    'XL': ShipStationItem(sku=os.getenv('SHIRT_XL_SKU', '605925771884'))
+    'S':  ShipStationItem(sku=os.getenv('SHIRT_S_SKU', '605925771853'),quantity=1),
+    'M':  ShipStationItem(sku=os.getenv('SHIRT_M_SKU', '605925771860'),quantity=1),
+    'L':  ShipStationItem(sku=os.getenv('SHIRT_L_SKU', '605925771877'),quantity=1),
+    'XL': ShipStationItem(sku=os.getenv('SHIRT_XL_SKU', '605925771884'),quantity=1)
 }
 
 
@@ -30,7 +30,8 @@ def tshirt_cannon():
     )
     ss_order.set_billing_address(shipping_address)
     ss_order.set_shipping_address(shipping_address)
+    shirts[body['TShirtSize']].set_weight(no_weight)
     ss_order.add_item(shirts[body['TShirtSize']])
     ss.add_order(ss_order)
     ss.submit_orders()
-    return
+    return 'k bro'
